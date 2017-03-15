fdf4fa9f119b664568300cf8ed18d84bc995a24b
chengjin005@163.com <cj252664787>
2017-03-15 10:58:12 +0800

    导航菜单数据源

---
+++ app/portal/api/NavMenuApi.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\portal\api;
+
+use app\portal\model\NavMenuModel;
+
+class NavApi
+{
+    // 分类列表 用于模板设计
+    public function index($param = [])
+    {
+        $navMenuModel = new NavMenuModel();
+
+        $where = [];
+
+        if (!empty($param['keyword'])) {
+            $where['name'] = ['like', "%{$param['keyword']}%"];
+        }
+        if (!empty($param['id'])) {
+            $where['nav_id'] = $param['id'];
+        }
+
+        return $navMenuModel->where($where)->select();
+    }
+
+}
