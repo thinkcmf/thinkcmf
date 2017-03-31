31cb0625e4765c44dafdd1afa01e741121d07584
iyting <iyting@foxmail.com>
2017-03-31 14:37:25 +0800

    新增操作状态提示类

---
+++ app/portal/controller/ErrorCodeController.php
@@ -0,0 +1,18 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: iyting < iyting@fixmail.com>
+// +----------------------------------------------------------------------
+namespace app\portal\controller;
+
+class ErrorCode
+{
+	const DELETE_SUCCESS = '删除成功';
+	const DELETE_FAILED  = '删除失败';
+	const ADD_SUCCESS    = '添加成功';
+	const ADD_FAILED     = '添加失败';
+}
+
