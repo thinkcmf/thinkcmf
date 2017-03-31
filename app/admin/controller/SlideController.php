583561562e38850dd23fe15ac93a7859554f5e8f
小夏 <449134904@qq.com>
2017-03-31 11:21:04 +0800

    todo 幻灯片分类删除

--- app/admin/controller/SlideController.php
+++ app/admin/controller/SlideController.php
@@ -18,14 +18,16 @@ class SlideController extends AdminBaseController
     {
         $slidePostModel = new SlideModel();
         $list           = $slidePostModel->select();
-        $this->assign('list',$list);
+        $this->assign('list', $list);
         return $this->fetch();
     }
+
     //  幻灯片分类
     public function add()
     {
         return $this->fetch();
     }
+
     //幻灯片分类提交
     public function addPost()
     {
@@ -37,25 +39,38 @@ class SlideController extends AdminBaseController
         }
         $this->success("添加成功！", url("slide/index"));
     }
+
     //分类编辑
     public function edit()
     {
-        $id= $this->request->param('id');
+        $id             = $this->request->param('id');
         $slidePostModel = new SlideModel();
-        $result = $slidePostModel->where('id', $id)->find();
-        $this->assign('result',$result);
+        $result         = $slidePostModel->where('id', $id)->find();
+        $this->assign('result', $result);
         return $this->fetch();
     }
+
     //分类编辑提交
     public function editPost()
     {
         $data           = $this->request->param();
-//        print_r($data);exit;
         $slidePostModel = new SlideModel();
-        $result         = $slidePostModel->validate(true)->save($data,['id'=>$data['id']]);
+        $result         = $slidePostModel->validate(true)->save($data, ['id' => $data['id']]);
         if ($result === false) {
             $this->error($slidePostModel->getError());
         }
         $this->success("修改成功！", url("slide/index"));
+    }
+
+    //删除幻灯片
+    public function delete()
+    {
+        $id             = $this->request->param();
+        $result = SlideModel::destroy($id);
+        if ($result) {
+            $this->success("删除成功！", url("slide/index"));
+        }else{
+            $this->error("删除失败！");
+        }
     }
 }
