e74bb432bc67c8df23d80179b1924796c94a09df
zxxjjforever <zxxjjforever@163.com>
2017-03-31 13:11:47 +0800

    完善友情链接

---
+++ app/admin/validate/LinkValidate.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 小夏 < 449134904@qq.com>
+// +----------------------------------------------------------------------
+namespace app\admin\validate;
+
+use think\Validate;
+
+class LinkValidate extends Validate
+{
+    protected $rule = [
+        'name' => 'require',
+        'url'  => 'require',
+    ];
+
+    protected $message = [
+        'name.require' => '名称不能为空',
+        'url.require'  => '链接地址不能为',
+    ];
+
+}
