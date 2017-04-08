51e11c050a98ace65852b9d7aacda75b3e2ca6ad
zxxjjforever <zxxjjforever@163.com>
2017-04-08 10:06:23 +0800

    增加门户应用公共语言包

---
+++ app/portal/lang/common/en-us.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'TABLE_PORTAL_CATEGORY' => 'Article Category',
+    'TABLE_PORTAL_POST' => 'Article'
+];
---
+++ app/portal/lang/common/zh-cn.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'TABLE_PORTAL_CATEGORY' => '文章分类',
+    'TABLE_PORTAL_POST' => '文章'
+];
