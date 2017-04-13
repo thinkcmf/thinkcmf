e4edb17fcba28a3d3101f136ea845df8f58a18b4
zxxjjforever <zxxjjforever@163.com>
2017-04-13 16:03:26 +0800

    清空 app/common.php

--- app/common.php
+++ app/common.php
@@ -8,49 +8,4 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
-/**
- * @param string $imgParam
- * @param string $imgAttr
- * @return string
- */
-function CmfVerifyCodeImg($imgParam = 'length=4&font_size=20&width=238&height=50&use_curve=1&use_noise=1', $imgAttr = 'style="cursor: pointer;" title="点击获取"')
-{
-//    $src = ROOT_PATH . "/index.php?g=api&m=check_code&a=index&" . $imgParam;
-//    print_r($src);exit;
-    $img = <<<hello
-<img class="verify_img" src="$src" onclick="this.src='$src&time='+Math.random();" $imgAttr/>
-hello;
-    return $img;
-}
-
-/**
- * 获取客户端IP地址
- * @param integer $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
- * @param boolean $adv 是否进行高级模式获取（有可能被伪装）
- * @return mixed
- */
-function get_client_ip($type = 0, $adv = false)
-{
-    $type = $type ? 1 : 0;
-    static $ip = NULL;
-    if ($ip !== NULL) return $ip[$type];
-    if ($adv) {
-        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            $pos = array_search('unknown', $arr);
-            if (false !== $pos) unset($arr[$pos]);
-            $ip = trim($arr[0]);
-        } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
-    // IP地址合法验证
-    $long = sprintf("%u", ip2long($ip));
-    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
-    return $ip[$type];
-}
 
