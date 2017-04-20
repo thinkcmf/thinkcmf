0f6a8ae2e0ca6d793e0bf54dd036acf1fbb69a3d
小 夏 <449134904@qq.com>
2017-04-20 22:42:05 +0800

    用户文章发布 TODO

--- app/user/controller/ArticlesController.php
+++ app/user/controller/ArticlesController.php
@@ -8,11 +8,17 @@
 // +----------------------------------------------------------------------
 namespace app\user\controller;
 
+use think\Db;
 use cmf\controller\HomeBaseController;
 
 class ArticlesController extends HomeBaseController
 {
     public function index(){
+        //获取登录会员信息
+        $user= cmf_get_current_user();
+//        $result = Db::name('portal_post')->where('user_id',$user['id'])->select();
+//        print_r($result);exit;
+        $this->assign('user_id',$user['id']);
         return $this->fetch();
     }
 }
