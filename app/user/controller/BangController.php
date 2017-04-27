bb1668a152ea7231b5b308251a4689d92a7661b9
Powerless <wzxaini9@gmail.com>
2017-04-27 19:12:35 +0800

    绑定调整---未完成

---
+++ app/user/controller/BangController.php
@@ -0,0 +1,67 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Powerless < wzxaini9@gmail.com>
+// +----------------------------------------------------------------------
+namespace app\user\controller;
+
+use cmf\controller\UserBaseController;
+use app\user\model\UserModel;
+
+
+class BangController extends UserBaseController
+{
+
+    /**
+     * 绑定账户资料
+     */
+    public function index()
+    {
+        $editData = new UserModel();
+        $data = $editData->favorites();
+        $user = cmf_get_current_user();
+        $this->assign($user);
+        $this->assign("page", $data['page']);
+        $this->assign("lists", $data['lists']);
+        return $this->fetch("profile/bang");
+    }
+
+    public function mobile()
+    {
+        $editData = new UserModel();
+        $data = $editData->favorites();
+        $user = cmf_get_current_user();
+        $this->assign($user);
+        $this->assign("page", $data['page']);
+        $this->assign("lists", $data['lists']);
+        return $this->fetch("profile/bang");
+    }
+
+    public function email()
+    {
+        $editData = new UserModel();
+        $data = $editData->favorites();
+        $user = cmf_get_current_user();
+        $this->assign($user);
+        $this->assign("page", $data['page']);
+        $this->assign("lists", $data['lists']);
+        return $this->fetch("profile/bang");
+    }
+
+    public function oauth()
+    {
+        $oauth_user_model = M("OauthUser");
+        $uid              = sp_get_current_userid();
+        $oauths           = $oauth_user_model->where(["uid" => $uid])->select();
+        $new_oauths       = [];
+        foreach ($oauths as $oa) {
+            $new_oauths[strtolower($oa['from'])] = $oa;
+        }
+        $this->assign("oauths", $new_oauths);
+        return $this->fetch();
+    }
+
+}
