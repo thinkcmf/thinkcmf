a7e46e07261413de63e374db4fe8bfeda80f2b60
zxxjjforever <zxxjjforever@163.com>
2017-05-06 15:29:22 +0800

    修复导航数据源数据返回为空时报错

--- app/admin/model/NavMenuModel.php
+++ app/admin/model/NavMenuModel.php
@@ -133,11 +133,15 @@ class NavMenuModel extends Model
                                         $navData = null;
                                     }
 
-                                    $this->parseNavData($navData, $navApi);
+                                    if (!empty($navData) && !empty($navData['rule']) && count($navData['items']) > 0) {
+                                        $this->parseNavData($navData, $navApi);
 
-                                    if (!empty($navData)) {
-                                        array_push($navs, $navData);
+                                        if (!empty($navData['items'])) {
+                                            array_push($navs, $navData);
+                                        }
                                     }
+
+
                                 }
 
                             }
