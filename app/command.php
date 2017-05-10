6db9540bb6cd67422111ff46d68e7c5ab3e2d8a8
zxxjjforever <zxxjjforever@163.com>
2017-05-10 16:48:05 +0800

    增加 command 跨应用自动加载

--- app/command.php
+++ app/command.php
@@ -7,4 +7,19 @@
 // | Author: Dean <zxxjjforever@163.com>
 // +----------------------------------------------------------------------
 
-return [];
+$apps = cmf_scan_dir(APP_PATH . '*', GLOB_ONLYDIR);
+
+$returnCommands = [];
+
+foreach ($apps as $app) {
+    $commandFile = APP_PATH . $app . '/command.php';
+
+    if (file_exists($commandFile)) {
+        $commands       = include $commandFile;
+
+        $returnCommands = array_merge($returnCommands, $commands);
+    }
+
+}
+
+return $returnCommands;
