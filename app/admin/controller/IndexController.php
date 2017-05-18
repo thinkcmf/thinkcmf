3a46103134eabfa07c7d6439920e95a2f818c63b
zxxjjforever <zxxjjforever@163.com>
2017-05-18 15:58:03 +0800

    修复登录判断错误

--- app/admin/controller/IndexController.php
+++ app/admin/controller/IndexController.php
@@ -18,7 +18,7 @@ class IndexController extends AdminBaseController
     public function _initialize()
     {
         $adminSettings = cmf_get_option('admin_settings');
-        if (!empty($adminSettings['admin_password']) && $this->request->path() == $adminSettings['admin_password']) {
+        if (empty($adminSettings['admin_password']) || $this->request->path() == $adminSettings['admin_password']) {
             session("__LOGIN_BY_CMF_ADMIN_PW__", 1);//设置后台登录加密码
         }
 
