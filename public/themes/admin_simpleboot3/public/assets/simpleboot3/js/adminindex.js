96088c86312d02868bbc1b49a5e91dde1edebf00
zxxjjforever <zxxjjforever@163.com>
2017-05-19 16:44:07 +0800

    优化后台首页选项卡

--- public/themes/admin_simpleboot3/public/assets/simpleboot3/js/adminindex.js
+++ public/themes/admin_simpleboot3/public/assets/simpleboot3/js/adminindex.js
@@ -1,144 +1,81 @@
-$task_content_inner = null;
-$mainiframe=null;
-var tabwidth=118;
-$loading=null;
-$nav_wraper=$("#nav_wraper");
+var $taskContentInner = null;
+var $mainIframe       = null;
+var tabwidth          = 118;
+var $loading          = null;
+var $navWraper        = $("#nav-wrapper");
+var $content;
 $(function () {
-	$mainiframe=$("#mainiframe");
-	$content=$("#content");
-	$loading=$("#loading");
-	var headerheight=48;
-	$content.height($(window).height()-headerheight);
-	
-	
-	$nav_wraper.height($(window).height()-48-40);
-	$nav_wraper.css("overflow","auto");
-	//$nav_wraper.niceScroll();
-	$(window).resize(function(){
-		$nav_wraper.height($(window).height()-48-40);
-		$content.height($(window).height()-headerheight);
-		 calcTaskitemsWidth();
-	});
-	$("#content iframe").load(function(){
-    	$loading.hide();
-    });
-	
-    $task_content_inner = $("#task-content-inner");
-   
+    $mainIframe      = $("#mainiframe");
+    $content         = $("#content");
+    $loading         = $("#loading");
+    var headerHeight = 48;
+    $content.height($(window).height() - headerHeight);
 
-    $("#searchMenuKeyWord").keyup(function () {
-        var wd = $(this).val();
-        //searchedmenus
-        var $tmp = $("<div></div>");
-        if (wd != "") {
-            $("#allmenus a:contains('" + wd + "')").each(
-        function () {
-            $clone = $(this).clone().prepend('<img src="/images/left/01/note.png">');
-
-            $clone.wrapAll('<div class="menuitemsbig"></div>').parent().attr("onclick", $clone.attr("onclick")).appendTo($tmp);
-
-        }
-        );
-            $("#searchedmenus").html($tmp.html());
-            $("#searchedmenus").show();
-            $("#allmenus").hide();
-            $("#defaultstartmenu").hide();
-            $("#allmenuslink .menu_item_linkbutton").html("返回");
-            isAllDefault = false;
-            // $("#searchedmenus").html($tmp).show();
-
-        }
-
+    $navWraper.height($(window).height() - 48 - 40);
+    $navWraper.css("overflow", "auto");
+    $(window).resize(function () {
+        $navWraper.height($(window).height() - 48 - 40);
+        $content.height($(window).height() - headerHeight);
+        calcTaskContentWidth();
     });
 
-    
+    $("#content iframe").load(function () {
+        $loading.hide();
+    });
 
-    $("#appbox  li .delete").click(function (e) {
-        $(this).parent().remove();
+    $taskContentInner = $("#task-content-inner");
+
+    $("#task-content-inner").on("click", "li", function () {
+        openapp($(this).attr("app-url"), $(this).attr("app-id"), $(this).attr("app-name"));
         return false;
     });
 
-   
+    $("#task-content-inner").on("dblclick", "li", function () {
+        closeapp($(this));
+        return false;
 
-    ///
-
-    $(".apps_container").on("click","li", function () {
-        var app = '<li><span class="delete" style="display:inline">×</span><img src="" class="icon"><a href="#" class="title"></a></li>';
-        var $app = $(app);
-        $app.attr("data-appname", $(this).attr("data-appname"));
-        $app.attr("data-appid", $(this).attr("data-appid"));
-        $app.attr("data-appurl", $(this).attr("data-appurl"));
-        $app.find(".icon").attr("src", $(this).attr("data-icon"));
-        $app.find(".title").html($(this).attr("data-appname"));
-        $app.appendTo("#appbox");
-        $("#appbox  li .delete").off("click");
-        $("#appbox  li .delete").click(function () {
-            $(this).parent().remove();
-            return false;
-        });
     });
-
-    ///
-    $("#tdshortcutsmor1").click(function () {
-        $(".window").hide();
-    });
-
-    $(".task-item").on("click", function () {
-        var appid = $(this).attr("app-id");
-        var $app = $('#' + appid);
-        showTopWindow($app);
-    });
-
-    $("#task-content-inner").on("click","li", function () {
-    	openapp($(this).attr("app-url"), $(this).attr("app-id"), $(this).attr("app-name"));
-    	return false;
-    });
-    
-    $("#task-content-inner").on("dblclick","li", function () {
-    	closeapp($(this));
-    	return false;
-    	
-    });
-    $("#task-content-inner").on("click",".macro-component-tabclose",function () {
-    	closeapp($(this).parent());
+    $("#task-content-inner").on("click", ".cmf-component-tabclose", function () {
+        closeapp($(this).parent());
         return false;
     });
-    
+
     $("#task-next").click(function () {
-        var marginleft = $task_content_inner.css("margin-left");
-        marginleft = marginleft.replace("px", "");
-        var width = $("#task-content-inner li").length * tabwidth;
-        var content_width = $("#task-content").width();
-        var lesswidth = content_width - width;
-        marginleft = marginleft - tabwidth <= lesswidth ? lesswidth : marginleft - tabwidth;
+        var marginLeft   = $taskContentInner.css("margin-left");
+        marginLeft       = marginLeft.replace("px", "");
+        var contentInner = $("#task-content-inner").width();
+        var contentWidth = $("#task-content").width();
+        var lessWidth    = contentWidth - contentInner;
+        marginLeft       = marginLeft - tabwidth <= lessWidth ? lessWidth : marginLeft - tabwidth;
 
-        $task_content_inner.stop();
-        $task_content_inner.animate({ "margin-left": marginleft + "px" }, 300, 'swing');
+        $taskContentInner.stop();
+        $taskContentInner.animate({"margin-left": marginLeft + "px"}, 300, 'swing');
     });
+
     $("#task-pre").click(function () {
-        var marginleft = $task_content_inner.css("margin-left");
-        marginleft = parseInt(marginleft.replace("px", ""));
-        marginleft = marginleft + tabwidth > 0 ? 0 : marginleft + tabwidth;
-        // $task_content_inner.css("margin-left", marginleft + "px");
-        $task_content_inner.stop();
-        $task_content_inner.animate({ "margin-left": marginleft + "px" }, 300, 'swing');
-    });
-    
-    $("#refresh_wrapper").click(function(){
-    	var $current_iframe=$("#content iframe:visible");
-    	$loading.show();
-    	//$current_iframe.attr("src",$current_iframe.attr("src"));
-    	$current_iframe[0].contentWindow.location.reload();
-    	return false;
+        var marginLeft = $taskContentInner.css("margin-left");
+        marginLeft     = parseInt(marginLeft.replace("px", ""));
+        marginLeft     = marginLeft + tabwidth > 0 ? 0 : marginLeft + tabwidth;
+        // $taskContentInner.css("margin-left", marginLeft + "px");
+        $taskContentInner.stop();
+        $taskContentInner.animate({"margin-left": marginLeft + "px"}, 300, 'swing');
     });
 
-    calcTaskitemsWidth();
+    $("#refresh-wrapper").click(function () {
+        var $currentIframe = $("#content iframe:visible");
+        $loading.show();
+        //$currentIframe.attr("src",$currentIframe.attr("src"));
+        $currentIframe[0].contentWindow.location.reload();
+        return false;
+    });
+
+    calcTaskContentWidth();
 });
-function calcTaskitemsWidth() {
-    var width = $("#task-content-inner li").length * tabwidth;
-    $("#task-content-inner").width(width);
-    if (($(document).width()-268-tabwidth- 30 * 2) < width) {
-        $("#task-content").width($(document).width() -268-tabwidth- 30 * 2);
+
+function calcTaskContentWidth() {
+    var width = $("#task-content-inner").width();
+    if (($(document).width() - 268 - tabwidth - 30 * 2) < width) {
+        $("#task-content").width($(document).width() - 268 - tabwidth - 30 * 2);
         $("#task-next,#task-pre").show();
     } else {
         $("#task-next,#task-pre").hide();
@@ -146,103 +83,94 @@ function calcTaskitemsWidth() {
     }
 }
 
-function close_current_app(){
-	closeapp($("#task-content-inner .active"));
+function close_current_app() {
+    closeapp($("#task-content-inner .active"));
 }
 
-function closeapp($this){
-	if(!$this.is(".noclose")){
-		$this.prev().click();
-    	$this.remove();
-    	$("#appiframe-"+$this.attr("app-id")).remove();
-    	calcTaskitemsWidth();
-    	$("#task-next").click();
-	}
-	 
+function closeapp($this) {
+    if (!$this.is(".noclose")) {
+        $this.prev().click();
+        $this.remove();
+        $("#appiframe-" + $this.attr("app-id")).remove();
+        calcTaskContentWidth();
+        $("#task-next").click();
+    }
+
 }
 
 
+var task_item_tpl = '<li class="cmf-component-tabitem">' +
+    '<a class="cmf-tabs-item-text"></a>' +
+    '<span class="cmf-component-tabclose" href="javascript:void(0)" title="点击关闭标签"><span></span><b class="cmf-component-tabclose-icon">×</b></span>' +
+    '</li>';
 
+var appiframe_tpl = '<iframe style="width:100%;height: 100%;" frameborder="0" class="appiframe"></iframe>';
 
-
-var task_item_tpl ='<li class="macro-component-tabitem">'+
-'<a class="macro-tabs-item-text"></a>'+
-'<span class="macro-component-tabclose" href="javascript:void(0)" title="点击关闭标签"><span></span><b class="macro-component-tabclose-icon">×</b></span>'+
-'</li>';
-
-var appiframe_tpl='<iframe style="width:100%;height: 100%;" frameborder="0" class="appiframe"></iframe>';
-
-function openapp(url, appid, appname, refresh) {
-    var $app = $("#task-content-inner li[app-id='"+appid+"']");
+function openapp(url, appId, appname, refresh) {
+    var $app = $("#task-content-inner li[app-id='" + appId + "']");
     $("#task-content-inner .active").removeClass("active");
     if ($app.length == 0) {
-        var task = $(task_item_tpl).attr("app-id", appid).attr("app-url",url).attr("app-name",appname).addClass("active");
-        task.find(".macro-tabs-item-text").html(appname).attr("title",appname);
-        $task_content_inner.append(task);
+        var task = $(task_item_tpl).attr("app-id", appId).attr("app-url", url).attr("app-name", appname).addClass("active");
+        task.find(".cmf-tabs-item-text").html(appname).attr("title", appname);
+        $taskContentInner.append(task);
         $(".appiframe").hide();
         $loading.show();
-        $appiframe=$(appiframe_tpl).attr("src",url).attr("id","appiframe-"+appid);
+        $appiframe = $(appiframe_tpl).attr("src", url).attr("id", "appiframe-" + appId);
         $appiframe.appendTo("#content");
-        $appiframe.load(function(){
-        	$loading.hide();
+        $appiframe.load(function () {
+            $loading.hide();
         });
-        calcTaskitemsWidth();
+        calcTaskContentWidth();
     } else {
-    	$app.addClass("active");
-    	$(".appiframe").hide();
-    	var $iframe=$("#appiframe-"+appid);
-    	var src=$iframe.get(0).contentWindow.location.href;
-    	src=src.substr(src.indexOf("://")+3);
-    	/*if(src!=GV.HOST+url){
-    		$loading.show();
-    		$iframe.attr("src",url);
-    		$appiframe.load(function(){
-            	$loading.hide();
+        $app.addClass("active");
+        $(".appiframe").hide();
+        var $iframe = $("#appiframe-" + appId);
+        var src     = $iframe.get(0).contentWindow.location.href;
+        src         = src.substr(src.indexOf("://") + 3);
+        if (refresh === true) {//刷新
+            $loading.show();
+            $iframe.attr("src", url);
+            $iframe.load(function () {
+                $loading.hide();
             });
-    	}*/
-    	if(refresh===true){//刷新
-    		$loading.show();
-    		$iframe.attr("src",url);
-    		$iframe.load(function(){
-            	$loading.hide();
-            });
-    	}
-    	$iframe.show();
-    	//$mainiframe.attr("src",url);
+        }
+        $iframe.show();
     }
-    
-    //
-    var itemoffset= $("#task-content-inner li[app-id='"+appid+"']").index()* tabwidth;
-    var width = $("#task-content-inner li").length * tabwidth;
-   
-    var content_width = $("#task-content").width();
-    var offset=itemoffset+tabwidth-content_width;
-    
-    var lesswidth = content_width - width;
-    
-    var marginleft = $task_content_inner.css("margin-left");
-   
-    marginleft =parseInt( marginleft.replace("px", "") );
-    var copymarginleft=marginleft;
-    if(offset>0){
-    	marginleft=marginleft>-offset?-offset:marginleft;
-    }else{
-    	marginleft=itemoffset+marginleft>=0?marginleft:-itemoffset;
+
+    var taskContentInner = $("#task-content-inner").width();
+    var contentWidth     = $("#task-content").width();
+    if (taskContentInner <= contentWidth) { //如果没有开始滚动就不用进行下去了
+        return;
     }
-    
-    if(-itemoffset==marginleft){
-    	marginleft = marginleft + tabwidth > 0 ? 0 : marginleft + tabwidth;
+
+    var currentTabIndex = $("#task-content-inner li[app-id='" + appId + "']").index();
+    var itemOffset      = 0;
+    var currentTabWidth = $("#task-content-inner li[app-id='" + appId + "']").width();
+
+    $("#task-content-inner li:lt(" + currentTabIndex + ')').each(function () {
+        itemOffset = itemOffset + $(this).width();
+    });
+
+    var cssMarginLeft = $taskContentInner.css("margin-left");
+
+    cssMarginLeft = parseInt(cssMarginLeft.replace("px", ""));
+
+
+    var marginLeft = currentTabWidth + itemOffset - contentWidth + cssMarginLeft;
+
+    if (marginLeft > 0) {
+        marginLeft = -(currentTabWidth + itemOffset - contentWidth);
+        $taskContentInner.animate({"margin-left": marginLeft + "px"}, 300, 'swing');
+        return;
     }
-    
-    //alert("cddd:"+(content_width-copymarginleft)+" dddd:"+(-itemoffset));
-    if(content_width-copymarginleft-tabwidth==itemoffset){
-    	marginleft = marginleft - tabwidth <= lesswidth ? lesswidth : marginleft - tabwidth;
+
+    if (itemOffset + cssMarginLeft < 0) {
+        marginLeft = -itemOffset
+        $taskContentInner.animate({"margin-left": marginLeft + "px"}, 300, 'swing');
+
+        return;
     }
-    
-	$task_content_inner.animate({ "margin-left": marginleft + "px" }, 300, 'swing');
-    
-    
-    
-  
+
+
 }
 
