52f1b4a25b7e358d1ae44e1853a584e5578c2b8d
zxxjjforever <zxxjjforever@163.com>
2017-06-06 20:55:18 +0800

    完善 portal 应用版权信息

--- app/portal/lang/zh-cn/common.php
+++ app/portal/lang/zh-cn/common.php
@@ -1,6 +1,15 @@
 <?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
 return [
-    'TABLE_PORTAL_CATEGORY' => '文章分类',
-    'TABLE_PORTAL_POST' => '文章',
+    'TABLE_PORTAL_CATEGORY'  => '文章分类',
+    'TABLE_PORTAL_POST'      => '文章',
     'TABLE_PORTAL_POST#PAGE' => '页面'
 ];
