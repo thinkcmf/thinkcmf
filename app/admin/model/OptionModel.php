8522e8fadd5f1dd27bc34389ac8abaf5e37febee
zxxjjforever <zxxjjforever@163.com>
2017-06-06 20:03:35 +0800

    完善 admin 应用版权信息

--- app/admin/model/OptionModel.php
+++ app/admin/model/OptionModel.php
@@ -1,7 +1,18 @@
 <?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
 namespace app\admin\model;
+
 use think\Model;
 
-class OptionModel extends Model{
+class OptionModel extends Model
+{
 
 }
