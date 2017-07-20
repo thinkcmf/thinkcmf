7b9bb8dafd9e7f1962f0eaf639e663ca1010f299
zxxjjforever <zxxjjforever@163.com>
2017-07-20 22:44:21 +0800

    增加htmlpurifier 类库

--- simplewind/vendor/composer/autoload_files.php
+++ simplewind/vendor/composer/autoload_files.php
@@ -9,4 +9,5 @@ return array(
     '9b552a3cc426e3287cc811caefa3cf53' => $vendorDir . '/topthink/think-helper/src/helper.php',
     '1cfd2761b63b0a29ed23657ea394cb2d' => $vendorDir . '/topthink/think-captcha/src/helper.php',
     '841780ea2e1d6545ea3a253239d59c05' => $vendorDir . '/qiniu/php-sdk/src/Qiniu/functions.php',
+    '2cffec82183ee1cea088009cef9a6fc3' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
 );
