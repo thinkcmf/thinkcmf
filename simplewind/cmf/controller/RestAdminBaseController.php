1ee4d0c31bca1c2991e19ab1ffe61de1d550b14a
zxxjjforever <zxxjjforever@163.com>
2017-08-23 23:09:59 +0800

    优化api用户登录验证

--- simplewind/cmf/controller/RestAdminBaseController.php
+++ simplewind/cmf/controller/RestAdminBaseController.php
@@ -10,43 +10,15 @@
 // +----------------------------------------------------------------------
 namespace cmf\controller;
 
-use think\Db;
-
 class RestAdminBaseController extends RestBaseController
 {
     public function _initialize()
     {
-
-        $token      = $this->request->header('XX-Token');
-        $deviceType = $this->request->header('XX-Device-Type');
-
-        if (empty($token)) {
-            $this->error(['code' => 10001, 'msg' => 'Token不能为空']);
-        }
-
-        if (empty($deviceType)) {
-            $this->error(['code' => 10001, 'msg' => '设备类型不能为空']);
-        }
-
-        if (!in_array($deviceType, $this->allowedDeviceTypes)) {
-            $this->error(['code' => 10001, 'msg' => '设备类型不存在!']);
-        }
-
-        $this->token      = $token;
-        $this->deviceType = $deviceType;
-
-        $user = Db::name('user_token')
-            ->alias('a')
-            ->field('b.*')
-            ->where(['token' => $token, 'device_type' => $deviceType])
-            ->join('__USER__ b', 'a.user_id = b.id')
-            ->find();
-
-        if (empty($user)) {
+        if (empty($this->user)) {
+            $this->error(['code' => 10001, 'msg' => '登录已失效!']);
+        } elseif ($this->userType != 1) {
             $this->error(['code' => 10001, 'msg' => '登录已失效!']);
         }
-
-        $this->userId = $user['id'];
     }
 
 
