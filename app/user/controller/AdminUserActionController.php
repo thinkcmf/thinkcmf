5e4778bbf3da785bbb7e5ff7c2ae72e44049d88a
zxxjjforever <zxxjjforever@163.com>
2017-09-12 13:40:07 +0800

    增加用户操作同步功能 #291

--- app/user/controller/AdminUserActionController.php
+++ app/user/controller/AdminUserActionController.php
@@ -60,6 +60,19 @@ class AdminUserActionController extends AdminBaseController
         return $this->fetch();
     }
 
+    /**
+     * 编辑用户操作
+     * @adminMenu(
+     *     'name'   => '编辑用户操作',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> true,
+     *     'order'  => 10000,
+     *     'icon'   => '',
+     *     'remark' => '编辑用户操作',
+     *     'param'  => ''
+     * )
+     */
     public function edit()
     {
         $id     = $this->request->param('id', 0, 'intval');
@@ -69,6 +82,19 @@ class AdminUserActionController extends AdminBaseController
         return $this->fetch();
     }
 
+    /**
+     * 编辑用户操作提交
+     * @adminMenu(
+     *     'name'   => '编辑用户操作提交',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10000,
+     *     'icon'   => '',
+     *     'remark' => '编辑用户操作提交',
+     *     'param'  => ''
+     * )
+     */
     public function editPost()
     {
         $id = $this->request->param('id', 0, 'intval');
@@ -82,5 +108,67 @@ class AdminUserActionController extends AdminBaseController
 
         $this->success('保存成功！');
     }
+
+    /**
+     * 同步用户操作
+     * @adminMenu(
+     *     'name'   => '同步用户操作',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> true,
+     *     'order'  => 10000,
+     *     'icon'   => '',
+     *     'remark' => '同步用户操作',
+     *     'param'  => ''
+     * )
+     */
+    public function sync()
+    {
+
+        $apps = cmf_scan_dir(APP_PATH . '*', GLOB_ONLYDIR);
+
+        foreach ($apps as $app) {
+            $userActionConfigFile = APP_PATH . $app . '/user_action.php';
+            if (file_exists($userActionConfigFile)) {
+                $userActionsInFile = include $userActionConfigFile;
+
+                foreach ($userActionsInFile as $userActionKey => $userAction) {
+
+                    $userAction['cycle_type'] = empty($userAction['cycle_type']) ? 0 : $userAction['cycle_type'];
+
+                    if (!in_array($userAction['cycle_type'], [0, 1, 2, 3])) {
+                        $userAction['cycle_type'] = 0;
+                    }
+
+                    if (!empty($userAction['url']) && is_array($userAction['url']) && !empty($userAction['url']['action'])) {
+                        $userAction['url'] = json_encode($userAction['url']);
+                    } else {
+                        $userAction['url'] = '';
+                    }
+
+                    $findUserAction = Db::name('user_action')->where(['action' => $userActionKey])->count();
+
+                    $userAction['app'] = $app;
+
+                    if ($findUserAction > 0) {
+                        Db::name('user_action')->where(['action' => $userActionKey])
+                            ->strict(false)->field(true)
+                            ->update([
+                                'name' => $userAction['name'],
+                                'url'  => $userAction['url']
+                            ]);
+                    } else {
+                        $userAction['action'] = $userActionKey;
+                        Db::name('user_action')->strict(false)
+                            ->field(true)
+                            ->insert($userAction);
+                    }
+                }
+            }
+        }
+
+        return $this->fetch();
+    }
+
 
 }
