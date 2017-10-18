a0534698d07dae050dcec6793f0a778ba40f86ee
zxxjjforever <zxxjjforever@163.com>
2017-10-18 16:43:14 +0800

    增加 api 命名空间

--- app/config.php
+++ app/config.php
@@ -26,7 +26,7 @@ $configs = [
     // 入口自动绑定模块
     'auto_bind_module'        => false,
     // 注册的根命名空间
-    'root_namespace'          => ['cmf' => CMF_PATH, 'plugins' => PLUGINS_PATH],
+    'root_namespace'          => ['cmf' => CMF_PATH, 'plugins' => PLUGINS_PATH, 'api' => CMF_ROOT . 'api/'],
     // 扩展函数文件
     'extra_file_list'         => [CMF_PATH . 'common' . EXT, THINK_PATH . 'helper' . EXT],
     // 默认输出类型
