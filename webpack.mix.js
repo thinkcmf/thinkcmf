5bcc177ff0524df906271b392d9b81bfa0db50fc
wanglelecc <wanglelecc@gmail.com>
2017-10-21 09:07:37 +0800

    添加注释及修复browserSync配置未注入webpack问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,10 @@ const { mix } = require('laravel-mix');
  */
 // mix.setPublicPath('public');
 
+// 预定义目录名称
 const assetsDir = 'assets/';
 
+// 设置预定义发布目录
 mix.setPublicPath('public').options({
     fileLoaderDirs: {
         images: assetsDir+'images',
@@ -21,13 +23,17 @@ mix.setPublicPath('public').options({
     }
 });
 
-mix.js('assets/js/app.js', assetsDir+'js').sass('assets/sass/app.scss', assetsDir+'css');
+// 加入需要发布的资源
+mix.js('assets/js/app.js', assetsDir+'js')
+.sass('assets/sass/app.scss', assetsDir+'css');
 
+// BrowserSync 自动更新服务配置
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 var browserSync = new BrowserSyncPlugin(
         Object.assign({
             port: 3000,
-            proxy: 'example.com',
+            proxy: 'example.com', // 这里修改成当前项目域名
+            // 这里配置监控目录，只要符合规则的文件被修改，会立即发布资源并刷新页面（只有在 npm run watch 模式下哦）
             files: [
                 'app/**/*.php',
                 'public/themes/**/*.html',
@@ -47,5 +53,7 @@ var browserSync = new BrowserSyncPlugin(
         { reload: false }
     );
 
-
-mix.disableNotifications();
+// 将 browserSync 配置注入到 webpack 配置
+mix.webpackConfig({
+    plugins : [browserSync]
+});
