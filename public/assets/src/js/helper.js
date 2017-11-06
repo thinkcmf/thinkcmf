efba064a175727df964ec5b69a7f42e5d0137f66
wanglelecc <wanglelecc@gmail.com>
2017-11-06 14:37:35 +0800

    修改忽略目录配置，加入新 assets 结构

---
+++ public/assets/src/js/helper.js
@@ -0,0 +1,16 @@
+/**
+ * Custom Helper
+ * wanglelecc
+ * https://github.com/wanglelecc/assets
+ */
+class helper{
+
+    static test(){
+        console.log('test...');
+        
+        return 'test';
+    }
+
+}
+
+module.exports = helper;
