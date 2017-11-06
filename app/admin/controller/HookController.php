6475e63d5b3ca8ddfee6c2454d3c68f328f033db
zxxjjforever <zxxjjforever@163.com>
2017-11-06 10:28:38 +0800

    优化钩子插件管理

--- app/admin/controller/HookController.php
+++ app/admin/controller/HookController.php
@@ -60,7 +60,11 @@ class HookController extends AdminBaseController
     {
         $hook        = $this->request->param('hook');
         $pluginModel = new PluginModel();
-        $plugins     = $pluginModel->field('a.*,b.hook,b.plugin,b.list_order,b.status as hook_plugin_status,b.id as hook_plugin_id')->alias('a')->join('__HOOK_PLUGIN__ b', 'a.name = b.plugin')->where('b.hook', $hook)->select();
+        $plugins     = $pluginModel->field('a.*,b.hook,b.plugin,b.list_order,b.status as hook_plugin_status,b.id as hook_plugin_id')
+            ->alias('a')->join('__HOOK_PLUGIN__ b', 'a.name = b.plugin')
+            ->where('b.hook', $hook)
+            ->order('b.list_order asc')
+            ->select();
         $this->assign('plugins', $plugins);
         return $this->fetch();
     }
