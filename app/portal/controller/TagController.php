e07fec2b3f4c1654196c1a0fa06ab9bde6b6f7ca
zxxjjforever <zxxjjforever@163.com>
2018-01-14 20:15:42 +0800

    优化标签控制器支持标签名

--- app/portal/controller/TagController.php
+++ app/portal/controller/TagController.php
@@ -17,10 +17,16 @@ class TagController extends HomeBaseController
 {
     public function index()
     {
-        $id             = $this->request->param('id', 0, 'intval');
+        $id             = $this->request->param('id');
+
         $portalTagModel = new PortalTagModel();
 
-        $tag = $portalTagModel->where('id', $id)->where('status', 1)->find();
+        if(is_numeric($id)){
+            $tag = $portalTagModel->where('id', $id)->where('status', 1)->find();
+        }else{
+            $tag = $portalTagModel->where('name', $id)->where('status', 1)->find();
+        }
+
 
         if (empty($tag)) {
             abort(404, '标签不存在!');
