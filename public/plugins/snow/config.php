c5716507f081394f6593d29c682ade2d040354e1
Powerless <wzxaini9@gmail.com>
2018-01-23 12:13:05 +0800

    前台雪花插件。页面飘雪

---
+++ public/plugins/snow/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'snow' => [// 在后台插件配置表单中的键名 ,会是config[text]
+        'title' => '雪花形状:', // 表单的label标题
+        'type' => 'text',// 表单的类型：text,password,textarea,checkbox,radio,select等
+        'value' => '❄',// 表单的默认值
+    ],
+];
+					
