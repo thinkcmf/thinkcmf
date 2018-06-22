ba9a3d42b71d0ba4abe0990d3eea81b443f21ccf
zxxjjforever <zxxjjforever@163.com>
2018-06-22 11:32:21 +0800

    数据库升级文件

--- update/update.sql
+++
@@ -1,4 +0,0 @@
--- 增缩略图字段
-ALTER TABLE `cmf_portal_post` ADD `thumbnail` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '缩略图' AFTER `post_source`;
-
-ALTER TABLE `cmf_auth_rule` CHANGE `app` `app` VARCHAR(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '规则所属app';
