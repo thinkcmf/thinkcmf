9ddde38d0a06f01ab103759b35c566bacfc04c93
zxxjjforever <zxxjjforever@163.com>
2018-08-13 10:54:42 +0800

    优化上传 js 代码

--- public/static/js/frontend.js
+++ public/static/js/frontend.js
@@ -1221,7 +1221,14 @@ function uploadOne(dialog_title, input_selector, filetype, extra_params, app) {
     openUploadDialog(dialog_title, function (dialog, files) {
         $(input_selector).val(files[0].filepath);
         $(input_selector + '-preview').attr('href', files[0].preview_url);
-        $(input_selector + '-name').val(files[0].name);
+
+        var $name = $(input_selector + '-name');
+        if ($name.is(':input')) {
+            $name.val(files[0].name);
+        } else {
+            $name.text(files[0].name);
+        }
+
     }, extra_params, 0, filetype, app);
 }
 
@@ -1236,7 +1243,13 @@ function uploadOneImage(dialog_title, input_selector, extra_params, app) {
     openUploadDialog(dialog_title, function (dialog, files) {
         $(input_selector).val(files[0].filepath);
         $(input_selector + '-preview').attr('src', files[0].preview_url);
-        $(input_selector + '-name').val(files[0].name);
+
+        var $name = $(input_selector + '-name');
+        if ($name.is(':input')) {
+            $name.val(files[0].name);
+        } else {
+            $name.text(files[0].name);
+        }
     }, extra_params, 0, 'image', app);
 }
 
