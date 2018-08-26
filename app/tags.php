650df3363a71353deb51721f475b1c02a0e48e15
zxxjjforever <zxxjjforever@163.com>
2018-08-27 07:21:37 +0800

    删除无用行为配置文件

--- app/tags.php
+++ app/tags.php
@@ -19,18 +19,18 @@ return [
     'app_begin'    => [
         'cmf\\behavior\\LangBehavior',
     ],
-    // 模块初始化
-    'module_init'  => [],
-    // 操作开始执行
-    'action_begin' => [],
-    // 视图内容过滤
-    'view_filter'  => [],
-    // 日志写入
-    'log_write'    => [],
-    //日志写入完成
-    'log_write_done'=>[],
-    // 应用结束
-    'app_end'      => [],
+//    // 模块初始化
+//    'module_init'  => [],
+//    // 操作开始执行
+//    'action_begin' => [],
+//    // 视图内容过滤
+//    'view_filter'  => [],
+//    // 日志写入
+//    'log_write'    => [],
+//    //日志写入完成
+//    'log_write_done'=>[],
+//    // 应用结束
+//    'app_end'      => [],
     // 应用开始
     'admin_init'   => [
         'cmf\\behavior\\AdminLangBehavior',
