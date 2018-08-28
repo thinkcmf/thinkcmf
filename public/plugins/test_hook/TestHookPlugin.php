8c240d585c36e2925eab0b7f2d5030d16b6fc957
zxxjjforever <zxxjjforever@163.com>
2018-08-28 08:59:02 +0800

    优化测试钩子插件

--- public/plugins/test_hook/TestHookPlugin.php
+++ public/plugins/test_hook/TestHookPlugin.php
@@ -24,7 +24,7 @@ class TestHookPlugin extends Plugin
         'author_url'  => 'http://www.thinkcmf.com'
     ];
 
-    public $hasAdmin = 1;//插件是否有后台管理界面
+    public $hasAdmin = 0;//插件是否有后台管理界面
 
     // 插件安装
     public function install()
@@ -41,103 +41,109 @@ class TestHookPlugin extends Plugin
     //admin_setting_site_view 钩子方法
     public function adminSettingSiteView($param)
     {
-        return "test";
+        return "test admin_setting_site_view";
     }
 
     //admin_setting_clear_cache_view 钩子方法
     public function adminSettingClearCacheView($param)
     {
-        return "test";
+        return "test admin_setting_clear_cache_view";
     }
 
     //admin_nav_index_view 钩子方法
     public function adminNavIndexView($param)
     {
-        return "test";
+        return "test admin_nav_index_view";
     }
 
     //admin_link_index_view 钩子方法
     public function adminLinkIndexView($param)
     {
-        return "test";
+        return "test admin_link_index_view";
     }
 
     //admin_slide_index_view 钩子方法
     public function adminSlideIndexView($param)
     {
-        return "test";
+        return "test admin_slide_index_view";
     }
 
     //admin_user_index_view 钩子方法
     public function adminUserIndexView($param)
     {
-        return "test";
+        return "test admin_user_index_view";
     }
 
     //admin_rbac_index_view 钩子方法
     public function adminRbacIndexView($param)
     {
-        return "test";
+        return "test admin_rbac_index_view";
     }
 
     //portal_admin_article_index_view 钩子方法
     public function portalAdminArticleIndexView($param)
     {
-        return "test";
+        return "test portal_admin_article_index_view";
     }
 
     //portal_admin_category_index_view 钩子方法
     public function portalAdminCategoryIndexView($param)
     {
-        return "test";
+        return "test portal_admin_category_index_view";
     }
 
     //portal_admin_page_index_view 钩子方法
     public function portalAdminPageIndexView($param)
     {
-        return "test";
+        return "test portal_admin_page_index_view";
     }
 
     //portal_admin_tag_index_view 钩子方法
     public function portalAdminTagIndexView($param)
     {
-        return "test";
+        return "test portal_admin_tag_index_view";
     }
 
     //user_admin_index_view 钩子方法
     public function userAdminIndexView($param)
     {
-        return "test";
+        return "test user_admin_index_view";
     }
 
     //user_admin_asset_index_view 钩子方法
     public function userAdminAssetIndexView($param)
     {
-        return "test";
+        return "test user_admin_asset_index_view";
     }
 
     //user_admin_oauth_index_view 钩子方法
     public function userAdminOauthIndexView($param)
     {
-        return "test";
+        return "test user_admin_oauth_index_view";
     }
 
     //admin_recycle_bin_index_view 钩子方法
     public function adminRecycleBinIndexView($param)
     {
-        return "test";
+        return "test admin_recycle_bin_index_view";
     }
 
     //admin_menu_index_view 钩子方法
     public function adminMenuIndexView($param)
     {
-        return "test";
+        return "test admin_menu_index_view";
+    }
+
+    //admin_index_index_view 钩子方法
+    public function adminIndexIndexView()
+    {
+        //return "test admin_index_index_view";
     }
 
     //admin_custom_login_open 钩子方法
-//    public function admin_custom_login_open($param)
-//    {
-//        return "test";
-//    }
+    public function admin_custom_login_open($param)
+    {
+        return false;
+    }
 
 }
