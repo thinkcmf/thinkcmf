04ce1068bb59c950bbb4cc9597a01bff5f6e5bee
迷人的小夏 <449134904@qq.com>
2018-12-21 18:44:26 +0800

    修改幻灯片查询条件

--- app/admin/controller/SlideController.php
+++ app/admin/controller/SlideController.php
@@ -39,7 +39,7 @@ class SlideController extends AdminBaseController
         }
 
         $slidePostModel = new SlideModel();
-        $slides         = $slidePostModel->where(['delete_time' => ['eq', 0]])->select();
+        $slides         = $slidePostModel->where('delete_time' ,'eq', 0)->select();
         $this->assign('slides', $slides);
         return $this->fetch();
     }
