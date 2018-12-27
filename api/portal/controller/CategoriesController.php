a555834e275d0c575783f592174ba915660a469d
迷人的小夏 <4491349047@qq.com>
2018-12-27 15:19:27 +0800

    修改new方式

--- api/portal/controller/CategoriesController.php
+++ api/portal/controller/CategoriesController.php
@@ -14,21 +14,17 @@ use api\portal\model\PortalCategoryModel;
 
 class CategoriesController extends RestBaseController
 {
-    protected $categoryModel;
-
-    public function __construct(PortalCategoryModel $categoryModel)
-    {
-        parent::__construct();
-        $this->categoryModel = $categoryModel;
-    }
-
     /**
      * 获取分类列表
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
      */
     public function index()
     {
-        $params = $this->request->get();
-        $data   = $this->categoryModel->getDatas($params);
+        $params        = $this->request->get();
+        $categoryModel = new PortalCategoryModel();
+        $data          = $categoryModel->getDatas($params);
 
         if (empty($this->apiVersion) || $this->apiVersion == '1.0.0') {
             $response = $data;
@@ -41,13 +37,17 @@ class CategoriesController extends RestBaseController
 
     /**
      * 显示指定的分类
-     * @param int $id
+     * @param $id
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
      */
     public function read($id)
     {
-        $params       = $this->request->get();
-        $params['id'] = $id;
-        $data         = $this->categoryModel->getDatas($params);
+        $params        = $this->request->get();
+        $params['id']  = $id;
+        $categoryModel = new PortalCategoryModel();
+        $data          = $categoryModel->getDatas($params);
         $this->success('请求成功!', $data);
     }
 
@@ -59,9 +59,9 @@ class CategoriesController extends RestBaseController
      */
     public function subCategories()
     {
-        $id = $this->request->get('category_id', 0, 'intval');
-
-        $categories = $this->categoryModel->where(['parent_id' => $id])->select();
+        $id            = $this->request->get('category_id', 0, 'intval');
+        $categoryModel = new PortalCategoryModel();
+        $categories    = $categoryModel->where(['parent_id' => $id])->select();
 
         $this->success('请求成功', ['categories' => $categories]);
 
