7688870af1163248c2bd87be54cda1b2f5b1ffd2
zxxjjforever <zxxjjforever@163.com>
2018-12-28 18:26:23 +0800

    增加phpoffice/phpspreadsheet库

--- simplewind/vendor/composer/autoload_psr4.php
+++ simplewind/vendor/composer/autoload_psr4.php
@@ -12,6 +12,9 @@ return array(
     'think\\' => array($vendorDir . '/topthink/think-image/src', $vendorDir . '/topthink/think-queue/src', $baseDir . '/simplewind/thinkphp/library/think'),
     'mindplay\\annotations\\' => array($vendorDir . '/mindplay/annotations/src/annotations'),
     'Qiniu\\' => array($vendorDir . '/qiniu/php-sdk/src/Qiniu'),
+    'Psr\\SimpleCache\\' => array($vendorDir . '/psr/simple-cache/src'),
+    'PhpOffice\\PhpSpreadsheet\\' => array($vendorDir . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet'),
     'FontLib\\' => array($vendorDir . '/phenx/php-font-lib/src/FontLib'),
     'Dompdf\\' => array($vendorDir . '/dompdf/dompdf/src'),
+    'Complex\\' => array($vendorDir . '/markbaker/complex/classes/src'),
 );
