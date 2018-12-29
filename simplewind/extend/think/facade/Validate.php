11b577e86ede8359aaed58ea3242f34c32460258
zxxjjforever <zxxjjforever@163.com>
2018-12-29 22:38:57 +0800

    优化验证类

--- simplewind/extend/think/facade/Validate.php
+++ simplewind/extend/think/facade/Validate.php
@@ -13,8 +13,30 @@ namespace think\facade;
 
 use think\Validate as ThinValidate;
 
+/**
+ * Class Validate
+ * @package think\facade
+ * @method bool is($value, $rule, $data = []) static 验证字段值是否为有效格式
+ * @method bool isDate($value) static 验证是否为有效的日期
+ * @method bool isEmail($value) static 验证是否为有效邮箱地址
+ */
 class Validate extends ThinValidate
 {
+    public static function __callStatic($method, $params)
+    {
+        $class = self::make();
+        if (method_exists($class, $method)) {
+            return call_user_func_array([$class, $method], $params);
+        } else if ('is' == strtolower(substr($method, 0, 2))) {
+            $method = substr($method, 2);
+            array_push($params, lcfirst($method));
+
+            return call_user_func_array([$class, 'is'], $params);
+        } else {
+            throw new \BadMethodCallException('method not exists:' . __CLASS__ . '->' . $method);
+        }
+
+    }
 
 
 }
