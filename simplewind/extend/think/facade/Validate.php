a9cdb629964256de65225974dbf994e441f29e62
zxxjjforever <zxxjjforever@163.com>
2018-12-29 17:25:39 +0800

    优化验证类

--- simplewind/extend/think/facade/Validate.php
+++ simplewind/extend/think/facade/Validate.php
@@ -11,34 +11,10 @@
 // ThinkPHP5.0兼容ThinkPHP5.1代码,用法请看ThinkPHP5.1文档
 namespace think\facade;
 
-use think\Validate as ThinkHook;
+use think\Validate as ThinValidate;
 
-class Validate
+class Validate extends ThinValidate
 {
-    /**
-     * 监听标签的行为
-     * @access public
-     * @param  string $tag    标签名称
-     * @param  mixed  $params 传入参数
-     * @param  bool   $once   只获取一个有效返回值
-     * @return mixed
-     */
-    public static function listen($tag, $params = null, $once = false)
-    {
-        return ThinkHook::listen($tag, $params, null, $once);
-    }
 
-    /**
-     * 动态添加行为扩展到某个标签
-     * @access public
-     * @param  string $tag      标签名称
-     * @param  mixed  $behavior 行为名称
-     * @param  bool   $first    是否放到开头执行
-     * @return void
-     */
-    public static function add($tag, $behavior, $first = false)
-    {
-        ThinkHook::add($tag, $behavior, $first);
-    }
 
 }
