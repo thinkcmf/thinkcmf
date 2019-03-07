1c350e0e52aaa0647826d4fd5ed60d4013dcb1b2
zxxjjforever <zxxjjforever@163.com>
2019-03-07 10:19:01 +0800

    升级 thinkcmf安装程序

--- vendor/thinkcmf/cmf-install/src/controller/IndexController.php
+++ vendor/thinkcmf/cmf-install/src/controller/IndexController.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
+// | Copyright (c) 2013-2019 http://www.thinkcmf.com All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -229,7 +229,6 @@ class IndexController extends BaseController
             return $this->fetch(":step4");
 
         } else {
-            exit;
         }
     }
 
