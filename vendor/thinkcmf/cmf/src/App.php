03008edeeb4b7e6203fa1e8f2bed8bb258dc3037
zxxjjforever <zxxjjforever@163.com>
2019-07-31 12:06:46 +0800

    优化app_init钩子

--- vendor/thinkcmf/cmf/src/App.php
+++ vendor/thinkcmf/cmf/src/App.php
@@ -274,6 +274,11 @@ class App extends Container
 
         // 路由初始化
         $this->routeInit();
+
+        if (PHP_SAPI != 'cli') {
+            // 监听app_init
+            $this->hook->listen('app_init');
+        }
     }
 
     /**
@@ -420,11 +425,6 @@ class App extends Container
         try {
             // 初始化应用
             $this->initialize();
-
-            if (!$this->initialized) {
-                // 监听app_init
-                $this->hook->listen('app_init');
-            }
 
             if ($this->bindModule) {
                 // 模块/控制器绑定
