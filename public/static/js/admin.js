af40bb7dd7f08d50f5f61f4de7f0dce1a5d02e89
惠达浪 <crazys@126.com>
2019-09-06 11:14:33 +0800

    修复变量定义bug，删除多余的console.log

--- public/static/js/admin.js
+++ public/static/js/admin.js
@@ -464,10 +464,9 @@
                 var $_this = this,
                     $this  = $($_this),
                     href   = $this.data('href'),
-                    msg    = $this.data('msg');
-                refresh    = $this.data('refresh');
+                    refresh= $this.data('refresh');
                 href       = href ? href : $this.attr('href');
-                refresh    = refresh == undefined ? 1 : refresh;
+                refresh    = refresh === undefined ? 1 : refresh;
 
 
                 $.getJSON(href).done(function (data) {
