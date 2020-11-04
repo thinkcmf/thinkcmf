fe7de1c0bb9e00e862796efcf4974026737f6c79
dean <zxxjjforever@163.com>
2020-11-04 19:44:11 +0800

    优化后台控制器

--- vendor/thinkcmf/cmf/src/controller/AdminBaseController.php
+++ vendor/thinkcmf/cmf/src/controller/AdminBaseController.php
@@ -20,21 +20,21 @@ class AdminBaseController extends BaseController
         // 监听admin_init
         hook('admin_init');
         parent::initialize();
-        $sessionAdminId = session('ADMIN_ID');
-        if (!empty($sessionAdminId)) {
-            $user = Db::name('user')->where('id', $sessionAdminId)->find();
-
-            if (!$this->checkAccess($sessionAdminId)) {
-                $this->error("您没有访问权限！");
-            }
-            $this->assign("admin", $user);
-        } else {
-            if ($this->request->isPost()) {
-                $this->error("您还没有登录！", url("admin/public/login"));
-            } else {
-                return $this->redirect(url("admin/Public/login"));
-            }
-        }
+//        $sessionAdminId = session('ADMIN_ID');
+//        if (!empty($sessionAdminId)) {
+//            $user = Db::name('user')->where('id', $sessionAdminId)->find();
+//
+//            if (!$this->checkAccess($sessionAdminId)) {
+//                $this->error("您没有访问权限！");
+//            }
+//            $this->assign("admin", $user);
+//        } else {
+//            if ($this->request->isPost()) {
+//                $this->error("您还没有登录！", url("admin/public/login"));
+//            } else {
+//                return $this->redirect(url("admin/Public/login"));
+//            }
+//        }
     }
 
     public function _initializeView()
@@ -65,8 +65,72 @@ class AdminBaseController extends BaseController
             ];
         }
 
-        config('template.view_base', WEB_ROOT . "$themePath/");
-        config('template.tpl_replace_string', $viewReplaceStr);
+        $this->view->engine()->config([
+            'view_base'          => $themePath . '/',
+            'tpl_replace_string' => $viewReplaceStr
+        ]);
+
+//        config('template.view_base', WEB_ROOT . "$themePath/");
+//        config('template.tpl_replace_string', $viewReplaceStr);
+    }
+
+    /**
+     * 加载模板输出
+     * @access protected
+     * @param string $template 模板文件名
+     * @param array  $vars     模板输出变量
+     * @param array  $config   模板参数
+     * @return mixed
+     */
+    protected function fetch($template = '', $vars = [], $config = [])
+    {
+        $template = $this->parseTemplate($template);
+        $content        = $this->view->fetch($template, $vars, $config);
+
+        return $content;
+    }
+
+    /**
+     * 自动定位模板文件
+     * @access private
+     * @param string $template 模板文件规则
+     * @return string
+     */
+    private function parseTemplate($template)
+    {
+        // 分析模板文件规则
+        $request = $this->request;
+        // 获取视图根目录
+        if (strpos($template, '@')) {
+            // 跨模块调用
+            list($module, $template) = explode('@', $template);
+        }
+
+        $cmfAdminThemePath    = config('template.cmf_admin_theme_path');
+        $cmfAdminDefaultTheme = cmf_get_current_admin_theme();
+        $themePath = "{$cmfAdminThemePath}{$cmfAdminDefaultTheme}/";
+
+        // 基础视图目录
+        $module = isset($module) ? $module : $this->app->http->getName();
+        $path   = $themePath . ($module ? $module . DIRECTORY_SEPARATOR : '');
+
+        $depr = config('view.view_depr');
+        if (0 !== strpos($template, '/')) {
+            $template   = str_replace(['/', ':'], $depr, $template);
+            $controller = cmf_parse_name($request->controller());
+            if ($controller) {
+                if ('' == $template) {
+                    // 如果模板文件名为空 按照默认规则定位
+                    $template = str_replace('.', DIRECTORY_SEPARATOR, $controller) . $depr . cmf_parse_name($request->action(true));
+                } elseif (false === strpos($template, $depr)) {
+                    $template = str_replace('.', DIRECTORY_SEPARATOR, $controller) . $depr . $template;
+                }
+            }
+        } else {
+            $template = str_replace(['/', ':'], $depr, substr($template, 1));
+        }
+
+        return $path . ltrim($template, '/') . '.' . ltrim(config('view.view_suffix'), '.');
     }
 
     /**
