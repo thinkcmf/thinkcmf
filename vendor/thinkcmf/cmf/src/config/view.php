a39f4ba791f4b610a8fefeb5792d013ac1a38583
dean <zxxjjforever@163.com>
2020-11-08 07:19:23 +0800

    取消模板默认过滤方法

--- vendor/thinkcmf/cmf/src/config/view.php
+++ vendor/thinkcmf/cmf/src/config/view.php
@@ -5,23 +5,25 @@
 
 return [
     // 模板引擎类型使用Think
-    'type'          => 'Think',
+    'type'            => 'Think',
     // 默认模板渲染规则 1 解析为小写+下划线 2 全部转换小写 3 保持操作方法
-    'auto_rule'     => 1,
+    'auto_rule'       => 1,
     // 模板目录名
-    'view_dir_name' => 'view',
+    'view_dir_name'   => 'view',
     // 模板后缀
-    'view_suffix'   => 'html',
+    'view_suffix'     => 'html',
     // 模板文件名分隔符
-    'view_depr'     => DIRECTORY_SEPARATOR,
+    'view_depr'       => DIRECTORY_SEPARATOR,
     // 模板引擎普通标签开始标记
-    'tpl_begin'     => '{',
+    'tpl_begin'       => '{',
     // 模板引擎普通标签结束标记
-    'tpl_end'       => '}',
+    'tpl_end'         => '}',
     // 标签库标签开始标记
-    'taglib_begin'  => '<',
+    'taglib_begin'    => '<',
     // 标签库标签结束标记
-    'taglib_end'    => '>',
+    'taglib_end'      => '>',
     // 内置标签库名称(标签使用不必指定标签库名称),以逗号分隔 注意解析顺序
-    'taglib_build_in'         => '\cmf\lib\taglib\Cmf,cx',
+    'taglib_build_in' => '\cmf\lib\taglib\Cmf,cx',
+    // 默认过滤方法 用于普通标签输出
+    'default_filter'  => ''
 ];
