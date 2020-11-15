423962c2c3e960ada89d24f9ce52ac1053b149df
dean <zxxjjforever@163.com>
2020-11-16 00:38:49 +0800

    删除app,api目录下的portal应用代码

--- vendor/thinkcmf/cmf-api/src/portal/controller/IndexController.php
+++ vendor/thinkcmf/cmf-api/src/portal/controller/IndexController.php
@@ -6,7 +6,7 @@
 // +----------------------------------------------------------------------
 // | Author: Dean <zxxjjforever@163.com>
 // +----------------------------------------------------------------------
-namespace api\home\controller;
+namespace api\portal\controller;
 
 use cmf\controller\RestBaseController;
 
@@ -16,7 +16,7 @@ class IndexController extends RestBaseController
     public function index()
     {
         $this->success("恭喜您,API访问成功!", [
-            'version' => '1.1.0',
+            'version' => '6.0.0',
             'doc'     => 'http://www.thinkcmf.com/cmf5api.html'
         ]);
     }
