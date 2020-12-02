e55a9cbf89f7faa71ee1db8d0570ac0644b39bdc
dean <zxxjjforever@163.com>
2020-12-02 08:03:29 +0800

    合并cmf6代码

---
+++ vendor/thinkcmf/cmf-app/src/admin/model/ThemeFileModel.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2019 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 小夏 < 449134904@qq.com>
+// +----------------------------------------------------------------------
+namespace app\admin\model;
+
+use think\Model;
+
+class ThemeFileModel extends Model
+{
+    /**
+     * 模型名称
+     * @var string
+     */
+    protected $name = 'theme_file';
+
+    protected $type = [
+        'more'        => 'array',
+        'config_more' => 'array',
+        'draft_more'  => 'array'
+    ];
+}
