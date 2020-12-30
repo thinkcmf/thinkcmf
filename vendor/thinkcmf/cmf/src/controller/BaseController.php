0a5eefa34dfec30e6b22faa102d13ed42640d599
dean <zxxjjforever@163.com>
2020-12-30 08:50:06 +0800

    增加validateFailError

--- vendor/thinkcmf/cmf/src/controller/BaseController.php
+++ vendor/thinkcmf/cmf/src/controller/BaseController.php
@@ -13,6 +13,8 @@ namespace cmf\controller;
 use think\Container;
 use think\Controller;
 use think\Db;
+use think\exception\HttpResponseException;
+use think\exception\ValidateException;
 use think\facade\View;
 use think\facade\Config;
 
@@ -74,6 +76,26 @@ class BaseController extends Controller
         }
 
         return true;
+    }
+
+    /**
+     * 验证数据并直接提示错误信息
+     * @access protected
+     * @param array        $data     数据
+     * @param string|array $validate 验证器名或者验证规则数组
+     * @param array        $message  提示信息
+     * @param mixed        $callback 回调方法（闭包）
+     * @return array|string|true
+     * @throws HttpResponseException
+     */
+    protected function validateFailError($data, $validate, $message = [], $callback = null)
+    {
+        $result = $this->validate($data, $validate, $message);
+        if ($result !== true) {
+            $this->error($result);
+        }
+
+        return $result;
     }
 
 }
