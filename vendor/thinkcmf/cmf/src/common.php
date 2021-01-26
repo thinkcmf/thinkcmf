7388f48862af1bb821a1a22f7276bdc097b1827b
dean <zxxjjforever@163.com>
2021-01-26 12:18:21 +0800

    优化db方法

--- vendor/thinkcmf/cmf/src/common.php
+++ vendor/thinkcmf/cmf/src/common.php
@@ -46,6 +46,20 @@ if (PHP_SAPI == 'cli') {
     }
 }
 
+if (!function_exists('db')) {
+    /**
+     * 实例化数据库类
+     * @param string        $name 操作的数据表名称（不含前缀）
+     * @param array|string  $config 数据库配置参数
+     * @param bool          $force 是否强制重新连接
+     * @return \think\db\Query
+     */
+    function db($name = '', $config = [], $force = false)
+    {
+        return Db::connect($config, $force)->name($name);
+    }
+}
+
 /**
  * 获取当前登录的管理员ID
  * @return int
