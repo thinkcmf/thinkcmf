2d76218e7a03377d022257bfc6e296811e128657
dean <zxxjjforever@163.com>
2021-04-24 15:07:19 +0800

    规范代码

--- app/demo/api/PageApi.php
+++ app/demo/api/PageApi.php
@@ -18,7 +18,7 @@ class PageApi
      */
     public function nav()
     {
-        $return = [
+        return [
             'rule'  => [
                 'action' => 'demo/Index/index',
                 'param'  => [
@@ -28,8 +28,6 @@ class PageApi
                 ['id' => 1, 'name' => 'test']
             ] //每个子项item里必须包括id,name,如果想表示层级关系请加上 parent_id
         ];
-
-        return $return;
     }
 
 }
