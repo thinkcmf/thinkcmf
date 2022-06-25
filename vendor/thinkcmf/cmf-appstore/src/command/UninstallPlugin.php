43a50de9eea2c40c40ea26542ee17a103cbb2855
dean <zxxjjforever@163.com>
2022-06-25 21:16:23 +0800

    版权

--- vendor/thinkcmf/cmf-appstore/src/command/UninstallPlugin.php
+++ vendor/thinkcmf/cmf-appstore/src/command/UninstallPlugin.php
@@ -1,5 +1,13 @@
 <?php
-
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-present http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <zxxjjforever@163.com>
+// +----------------------------------------------------------------------
 namespace app\admin\command;
 
 use app\admin\logic\PluginLogic;
