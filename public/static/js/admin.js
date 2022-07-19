ca4d1723bd0f7d031a6387f4f99812e91ac70631
dean <zxxjjforever@163.com>
2022-07-20 00:45:41 +0800

    优化imagePreviewDialog

--- public/static/js/admin.js
+++ public/static/js/admin.js
@@ -1068,30 +1068,31 @@ function uploadMultiFile(dialog_title, container_selector, item_tpl_wrapper_id, 
  * 查看图片对话框
  * @param img 图片地址
  */
-function imagePreviewDialog(img) {
+function imagePreviewDialog(img, options) {
     Wind.css('layer');
-
+    var params = {
+        photos: {
+            "title": "", //相册标题
+            "id": 'image_preview', //相册id
+            "start": 0, //初始显示的图片序号，默认0
+            "data": [   //相册包含的图片，数组格式
+                {
+                    "alt": "",
+                    "pid": 666, //图片id
+                    "src": img, //原图地址
+                    "thumb": img //缩略图地址
+                }
+            ]
+        } //格式见API文档手册页
+        , anim: 5, //0-6的选择，指定弹出图片动画类型，默认随机
+        shadeClose: true,
+        // skin: 'layui-layer-nobg',
+        shade: [0.5, '#000000'],
+        shadeClose: true,
+    };
+    params = options ? $.extend(params, options) : params;
     Wind.use("layer", function () {
-        layer.photos({
-            photos: {
-                "title": "", //相册标题
-                "id": 'image_preview', //相册id
-                "start": 0, //初始显示的图片序号，默认0
-                "data": [   //相册包含的图片，数组格式
-                    {
-                        "alt": "",
-                        "pid": 666, //图片id
-                        "src": img, //原图地址
-                        "thumb": img //缩略图地址
-                    }
-                ]
-            } //格式见API文档手册页
-            , anim: 5, //0-6的选择，指定弹出图片动画类型，默认随机
-            shadeClose: true,
-            // skin: 'layui-layer-nobg',
-            shade: [0.5, '#000000'],
-            shadeClose: true,
-        })
+        layer.photos(params)
     });
 }
 
