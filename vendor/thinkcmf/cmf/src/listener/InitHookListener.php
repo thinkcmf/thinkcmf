76347266a345c633df705460a713231f27ef66a1
dean <zxxjjforever@163.com>
2022-07-22 10:16:04 +0800

    优化语言包加载

--- vendor/thinkcmf/cmf/src/listener/InitHookListener.php
+++ vendor/thinkcmf/cmf/src/listener/InitHookListener.php
@@ -63,6 +63,21 @@ class InitHookListener
             }
         }
 
+        /**--start LangListener--------------------------------------*/
+        $this->app = app();
+        $langSet   = $this->app->lang->getLangSet();
 
+        $this->app->lang->load([
+            root_path() . "vendor/thinkcmf/cmf/src/lang/{$langSet}.php",
+        ]);
+
+        // 加载应用公共语言包
+        $apps = cmf_scan_dir($this->app->getAppPath() . '*', GLOB_ONLYDIR);
+        foreach ($apps as $app) {
+            $this->app->lang->load([
+                $this->app->getAppPath() . $app . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . $langSet . DIRECTORY_SEPARATOR . 'common.php',
+            ]);
+        }
+        /**--end LangListener--------------------------------------*/
     }
 }
