9d6621e0868940abbc74bd74fdb988d9b2298d71
五五 <15093565100@163.com>
2023-01-31 13:06:56 +0800

    添加 确实是 用户操作语言包

---
+++ vendor/thinkcmf/cmf-app/src/admin/lang/en-us/user_menu.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'USER_ADMINUSERACTION_INDEX' => 'User Operation Management',
+    'USER_ADMINUSERACTION_SYNC'  => 'User Action Synchronization'
+];
---
+++ vendor/thinkcmf/cmf-app/src/admin/lang/zh-cn/user_menu.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'USER_ADMINUSERACTION_INDEX' => '用户操作管理',
+    'USER_ADMINUSERACTION_SYNC'  => '用户操作同步'
+];
