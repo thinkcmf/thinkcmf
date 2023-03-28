cb11bfb77ea84397055fc9ca6811cf86e478373c
dean <zxxjjforever@163.com>
2023-03-28 22:01:03 +0800

    casbin

---
+++ vendor/thinkcmf/cmf/src/auth/model/AuthPolicyModel.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-present http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
+namespace cmf\auth\model;
+
+use think\Model;
+
+class AuthPolicyModel extends Model
+{
+    /**
+     * 模型名称
+     * @var string
+     */
+    protected $name = 'auth_policy';
+
+}
