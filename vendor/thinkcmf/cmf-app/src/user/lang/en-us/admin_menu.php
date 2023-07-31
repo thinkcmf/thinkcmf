cf202c35fed03573dd3a2d2effdeeb674b42c35d
dean <zxxjjforever@163.com>
2023-08-01 00:50:20 +0800

    修复语言包

--- vendor/thinkcmf/cmf-app/src/user/lang/en-us/admin_menu.php
+++ vendor/thinkcmf/cmf-app/src/user/lang/en-us/admin_menu.php
@@ -9,15 +9,16 @@
 // | Author: 老猫 <thinkcmf@126.com>
 // +----------------------------------------------------------------------
 return [
-    'USER_INDEXADMIN_BAN'       => 'Block User',
-    'USER_INDEXADMIN_CANCELBAN' => 'Active User',
-    'USER_INDEXADMIN_DEFAULT1'  => 'User Group',
-    'USER_INDEXADMIN_DEFAULT3'  => 'Admin Group',
-    'USER_INDEXADMIN_INDEX'     => 'Site Users',
-    'USER_OAUTHADMIN_DELETE'    => 'Unbind Thrid Party User',
-    'USER_OAUTHADMIN_INDEX'     => 'Third Party Users',
-    'USER_INDEXADMIN_DEFAULT'   => 'Users',
-    'USER_ADMINASSET_INDEX'     => 'Assets',
+    'USER_INDEXADMIN_BAN'        => 'Block User',
+    'USER_INDEXADMIN_CANCELBAN'  => 'Active User',
+    'USER_INDEXADMIN_DEFAULT1'   => 'User Group',
+    'USER_INDEXADMIN_DEFAULT3'   => 'Admin Group',
+    'USER_INDEXADMIN_INDEX'      => 'Site Users',
+    'USER_OAUTHADMIN_DELETE'     => 'Unbind Thrid Party User',
+    'USER_OAUTHADMIN_INDEX'      => 'Third Party Users',
+    'USER_INDEXADMIN_DEFAULT'    => 'Users',
+    'USER_ADMINASSET_INDEX'      => 'Assets',
     'USER_ADMINUSERACTION_INDEX' => 'User Operation Management',
-    'USER_ADMINUSERACTION_SYNC'  => 'User Action Synchronization'
+    'USER_ADMINUSERACTION_SYNC'  => 'User Operation Synchronization',
+    'USER_ADMINUSERACTION_EDIT'  => 'Edit User Operation'
 ];
