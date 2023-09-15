acfc3d9d2600f2fb86f08a1a479d6d55be4859a1
dean <zxxjjforever@163.com>
2023-09-15 16:47:12 +0800

    增加登录后返回用户信息

--- vendor/thinkcmf/cmf-app/src/admin/controller/PublicController.php
+++ vendor/thinkcmf/cmf-app/src/admin/controller/PublicController.php
@@ -108,7 +108,11 @@ class PublicController extends AdminBaseController
                 UserModel::where('id', $result['id'])->update($data);
                 cookie("admin_username", $name, 3600 * 24 * 30);
                 session("__LOGIN_BY_CMF_ADMIN_PW__", null);
-                $this->success(lang('LOGIN_SUCCESS'), url("admin/Index/index"), ['token' => $token]);
+                $this->success(lang('LOGIN_SUCCESS'), url("admin/Index/index"), ['token' => $token, 'user' => [
+                    'id'            => $result['id'],
+                    'user_login'    => $result['user_login'],
+                    'user_nickname' => $result['user_nickname'],
+                ]]);
             } else {
                 $this->error(lang('PASSWORD_NOT_RIGHT'));
             }
