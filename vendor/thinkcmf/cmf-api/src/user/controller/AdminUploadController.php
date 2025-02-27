732f30af5d5d1ef7da4f6b9d4fbcbb3a8b4cb9ed
wuwu <15093565100@163.com>
2025-02-27 20:39:29 +0800

    fix swagger api 注释 上传

--- vendor/thinkcmf/cmf-api/src/user/controller/AdminUploadController.php
+++ vendor/thinkcmf/cmf-api/src/user/controller/AdminUploadController.php
@@ -11,11 +11,47 @@ namespace api\user\controller;
 
 use cmf\controller\RestAdminBaseController;
 use api\user\traits\UploadTrait;
+use OpenApi\Annotations as OA;
 
-class UploadController extends RestAdminBaseController
+class AdminUploadController extends RestAdminBaseController
 {
     /**
      * 后台上传
+     * @OA\Post(
+     *      tags={"user"},
+     *      path="/admin/user/upload/one",
+     *      @OA\Parameter(
+     *         name="filetype",
+     *         description="文件类型（默认图片）",
+     *         in="query",
+     *         example="image"
+     *     ),
+     *     @OA\Parameter(
+     *         name="app",
+     *         description="应用code",
+     *         example="default",
+     *         in="query"
+     *     ),
+     *      @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                  @OA\Property(
+     *                      property="file",
+     *                      description="文件",
+     *                      type="string",
+     *                      format="binary"
+     *                  )
+     *             )
+     *         ),
+     *     ),
+     *      @OA\Response(
+     *           response="200",
+     *           @OA\JsonContent(example={"code": 1,"msg": "","data": {"filepath": "default/20250227/83f785dee2f083f18ba329da41f0e62d.png","name": "微信截图_20240716192358.png","preview_url": "http://test2.to/upload/default/20250227/83f785dee2f083f18ba329da41f0e62d.png","url": "http://test2.to/upload/default/20250227/83f785dee2f083f18ba329da41f0e62d.png","filename": "微信截图_20240716192358.png"}})
+     *      ),
+     *  )
+     *
      * @throws \think\Exception
      * @throws \think\db\exception\DataNotFoundException
      * @throws \think\db\exception\ModelNotFoundException
@@ -28,4 +64,5 @@ class UploadController extends RestAdminBaseController
     }
 
     use UploadTrait;
+
 }
